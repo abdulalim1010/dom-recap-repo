5f74e8c7ca02307ce833d612aee5b61163972774
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-03 21:49:51 +0600

    add new date

---
+++ correct.js
@@ -0,0 +1,5 @@
+const number = [3, 4, 63, 64,]
+const star=["ny one in the hard life in the bad situation so some is comming in our life "]
+console.log(Array.isArray (number));
+console.log(Array.isArray(star));
+isNaN(78);
