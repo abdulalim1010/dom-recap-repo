74c8cef721f94c009ef8f52e027f5f481412459c
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-04 22:57:38 +0600

    add summation

---
+++ arrow.js
@@ -0,0 +1,20 @@
+function sum(num1,num2) {
+  return num1 + num2;
+  
+}
+const add = sum(45, 67)
+console.log(add)
+const sum2=function(num1,num2) {
+  
+  return num1 + num2;
+  
+}
+console.log(sum2(56, 64));
+const a = 12;
+const b = 12;
+sum = a + b;
+console.log(sum);
+const sum3 = (num1, num2) => {
+ return  num1 + num2;
+} 
+console.log(sum3(6, 6));
