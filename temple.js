d30f645cd12929bc3d49eba90ab7a22d460c67e2
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-04 22:27:02 +0600

    last item

---
+++ temple.js
@@ -0,0 +1,5 @@
+const alim = "amar sonar bangla ami tomai valobasi chiro din tomar skasdn"
+
+const alime = "amra tomar sontan" + alim
+const dalim = `one upon a time there are some of people in our country live in the middle place some`+alime+alim
+console.log(dalim)
